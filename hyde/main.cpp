4bd97ea601951acab6d171de3cc2a777c75a607a
Branden <br66@njit,edu>
2015-01-23 14:09:41 -0500

    added comments

--- hyde/main.cpp
+++ hyde/main.cpp
@@ -1,15 +1,15 @@
 #include <SDL.h>
 
-int main( int argc, char *argv[] )
+int main( int argc, char *argv[] ) //sdl needs exact line to find and replace main function
 {
-	SDL_Surface *screen;
+	SDL_Surface *screen;  //declare surface
 
-	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Init(SDL_INIT_EVERYTHING); //start
 
-	screen = SDL_SetVideoMode(1600, 800, 32, SDL_SWSURFACE);
-	SDL_Delay(3200);
-	SDL_FreeSurface(screen);
+	screen = SDL_SetVideoMode(1600, 800, 32, SDL_SWSURFACE); //actually creates windows
+	SDL_Delay(3200); // timer 3.2 seconds
+	SDL_FreeSurface(screen); //memory mgmt - gets rid of surface
 
-	SDL_Quit();
+	SDL_Quit(); //end
 	return 0;
 }
