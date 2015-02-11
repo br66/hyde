097bf26041c807f38aca4d4924e102b81053bcdd
Branden <br66@njit,edu>
2015-02-10 19:11:42 -0500

    initial commit for entityStructure branch; will create entity and sprite structures

--- hyde/game.c
+++ hyde/game.c
@@ -18,7 +18,7 @@ extern SDL_Surface *dot = NULL;
 /*********************************
 FOR EVENTS LIKE KEYSTATES (PRESS AND RELEASE)
 *********************************/
-SDL_Event event;
+SDL_Event eventT;
 
 /*********************************
 FOR INITALIZING ALL TEH GRAPHIX
@@ -28,23 +28,56 @@ void Init_All(); //this function exists
 /*********************************
 FOR OBJECTS
 *********************************/
+//objTest_t *star;
 const int OBJTEST_WIDTH = 20;
 const int OBJTEST_HEIGHT = 20;
 
 /*********************************
-CLASSES FOR 
+FUNCTIONS FOR OBJTEST
 *********************************/
-/*class objTest
+void handle_Input(objTest_t *self);
+//void moveObject(objTest_t *self);
+//void showObject(objTest_t *self);
+
+void applySurface (int x, int y, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip)
 {
-private:
-	int x, y; //position
-	int xVel, yVel; //speeeeeeeeeeeeeeeeeeeeeeeeeed
+	//clip = NULL;
 
-public:
-	objTest();
+	SDL_Rect offset;
+
+    offset.x = x;
+    offset.y = y;
+
+    SDL_BlitSurface( source, NULL, destination, &offset );
+}
+
+/*void handle_Input(objTest_t *self) //shouldn't this be at the top of the file?? 
+{
+	//printf("handle input\n");
+	if ( eventT.type == SDL_KEYDOWN ) // << not doing this line
+	{
+		//printf("handle input\n");
+		switch( eventT.key.keysym.sym )
+		{
+			case SDLK_UP: self->yVel -= OBJTEST_HEIGHT / 2; break;
+			case SDLK_DOWN: self->yVel += OBJTEST_HEIGHT / 2; break;
+			case SDLK_LEFT: self->xVel -= OBJTEST_WIDTH / 2; break;
+			case SDLK_RIGHT: self->xVel += OBJTEST_WIDTH / 2; break;
+		}
+	}
+
+	else if ( eventT.type == SDL_KEYUP )
+	{
+
+		switch ( eventT.key.keysym.sym )
+		{
+			case SDLK_UP: self->yVel += OBJTEST_HEIGHT / 2; break;
+            case SDLK_DOWN: self->yVel -= OBJTEST_HEIGHT / 2; break;
+            case SDLK_LEFT: self->xVel += OBJTEST_WIDTH / 2; break;
+            case SDLK_RIGHT: self->xVel -= OBJTEST_WIDTH / 2; break;
+		}
+	}
 }*/
-
-
 
 int main(int argc, char *argv[]) 
 {	
@@ -58,11 +91,21 @@ int main(int argc, char *argv[])
 
 	Uint8 *keys;
 
+	//star->x = 0;
+	//star->y = 0;
+	//star->xVel = 0;
+	//star->yVel = 0;
+
+	//ZOMG JUS LIEK QUAEK TOOOOO
+	//star->handleInput = handle_Input; //just because i set it, does it mean it will happen?
+	//star->move = moveObject;
+	//star->show = showObject;
+
 	Init_All();
 
 	screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE); 
 
-	bg = SDL_LoadBMP("actionstashhd.bmp");
+	bg = SDL_LoadBMP("dot.bmp");
 
 	if(bg != NULL)
 	{
@@ -77,15 +120,29 @@ int main(int argc, char *argv[])
 /***********************************************************************************************************************/
 	do
 	{
+		//star->handleInput(star);
+		//star->move(star);
+		//star->show(star);
+		
 		ResetBuffer();
-		//DrawMouse();
 		NextFrame();
 		SDL_PumpEvents();
 
+		//applySurface(star->x, star->y, bg, screen, NULL);
+		//SDL_BlitSurface(bg,NULL,buffer,NULL);
+
 		keys = SDL_GetKeyState(&keyn);
 
-		if(keys[SDLK_UP])
-		if(keys[SDLK_ESCAPE])done = 1; //if i press escape, game over
+		//if(keys[SDLK_UP])
+		//{
+			//star->xVel += 2;
+			//star->x += star->xVel;
+			
+			//printf("guhhhh");
+		//}
+		
+		if(keys[SDLK_ESCAPE])done = 1;
+
 	}
 	while(!done); SDL_FreeSurface(screen); exit(0);
 	return 0;
@@ -98,6 +155,7 @@ void CleanUpAll()
   /*any other cleanup functions can be added here*/ 
 }
 
+
 void Init_All()
 {
   Init_Graphics();
@@ -105,3 +163,25 @@ void Init_All()
   //InitMouse();
   atexit(CleanUpAll); //when the program exits
 }
+
+/*void moveObject (objTest_t *self)
+{
+	self->x += self->xVel;
+
+	if ( ( self->x < 0 ) || ( self->x + OBJTEST_WIDTH > 640) )
+	{
+		self->x -= self->xVel;
+	}
+
+	self->y += self->yVel;
+	
+	if ( ( self->y < 0 ) || ( self->y + OBJTEST_HEIGHT > 480 ) )
+	{
+		self->y -= self->yVel;
+	}
+}
+
+void showObject (objTest_t *self)
+{
+	applySurface(self->x, self->y, dot, screen, NULL);
+}*/
--- hyde/objectTest.h
+++ hyde/objectTest.h
@@ -1,15 +1,20 @@
 //OH NO C DOESN'T HAVE THE CONCPET OF DATA HIDING!
 //Y? C Y?
+//FUNCTION POINTERS
 
 //CHANGED INCLUDE DIRECTORIES
 
-typedef struct
+
+
+typedef struct objTest_s
 {
 	int x, y;
 	int xVel, yVel;
 
-	void handle_input();
-	void move();
-	void show();
+	void (*handleInput) (struct objTest_s *self);
+	//void (*move) (objTest_t *self);
+	//void (*show) (objTest_t *self);
 
-} objTest;
+} objTest_t; //created an entiity type for entity
+
+
